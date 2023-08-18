a608a604ea49e8f86203264ab88dd7493bb55640
Yang Yanchao <116064365+13362130124@users.noreply.github.com>
2023-08-18 10:24:52 +0800

    add License Mulan PSL v2 (#5)
    * Correct use of gmemAdvise
    
    * add License

--- src/ascend/gmem_ascend.c
+++ src/ascend/gmem_ascend.c
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2023 yangyanchao6@huawei.com 
+ * libgmem is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -12,12 +24,12 @@ void init_gmemSemantics(void) __attribute__ ((constructor));
 
 int ascend_eager_free(void *userData, void *stream)
 {
-    return aclrtLaunchCallback((void (*)(void*))gmemHadvise, userData, ACL_CALLBACK_BLOCK, (aclrtStream)stream);
+    return aclrtLaunchCallback((void (*)(void*))gmemAdvise, userData, ACL_CALLBACK_BLOCK, (aclrtStream)stream);
 }
 
 int ascend_prefech(void *userData, void *stream)
 {
-    return aclrtLaunchCallback((void (*)(void*))gmemHadvise, userData, ACL_CALLBACK_BLOCK, (aclrtStream)stream);
+    return aclrtLaunchCallback((void (*)(void*))gmemAdvise, userData, ACL_CALLBACK_BLOCK, (aclrtStream)stream);
 }
 
 int ascend_numaid(void)
